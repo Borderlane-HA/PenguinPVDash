96930fe008660680c74eaa4885f6b26cfdf26095
Borderlane-HA <Spam-Schutz@gmx.de>
2025-08-23 08:31:25 +0200

    Update config.php

--- SERVER/inc/config.php
+++ SERVER/inc/config.php
@@ -2,8 +2,28 @@
 date_default_timezone_set('Europe/Berlin');
 $PVDASH_SQLITE = __DIR__ . '/../data/pvdash.sqlite';
 if (!is_dir(__DIR__ . '/../data')) { @mkdir(__DIR__ . '/../data', 0775, true); }
-$PVDASH_REQUIRE_AUTH = false;
-$PVDASH_API_KEYS = [ /* "home" => "HIERYOURAPIKEY" */ ];
+
+// Set Config start
+
+// false/true :: true if API Key set
+$PVDASH_REQUIRE_AUTH = true;
+// Device Name and API Key (Standard: home)
+$PVDASH_API_KEYS = [ /* "home" => "MYSECRETAPIKEY" */ ];
+// Language German (de) or english (en)
+$lang_from_config = 'de';
+
+// Set Config end
+
+$allowed = ['de','en'];
+$lang = $lang_from_config;
+if (isset($_GET['lang']) && in_array($_GET['lang'], $allowed, true)) {
+    $lang = $_GET['lang'];
+    setcookie('lang', $lang, time() + 31536000, '/'); // 1 Jahr
+} elseif (!empty($_COOKIE['lang']) && in_array($_COOKIE['lang'], $allowed, true)) {
+    $lang = $_COOKIE['lang'];
+}
+define('APP_LANG', $lang);
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, X-PVDash-Timestamp, X-PVDash-Device, X-PVDash-Signature');
 ?>
